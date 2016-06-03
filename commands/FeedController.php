7a280739f911beb89e65553913d1941640fd168c
Vladmir Kochergin <wawan-93@mail.ru>
2016-06-03 10:18:24 +0300

    #7 Exceptions when parse

--- commands/FeedController.php
+++ commands/FeedController.php
@@ -16,25 +16,31 @@ class FeedController extends Controller
         foreach ($feeds as $feed) {
             $last_feed_date = $last_modified =  new \DateTime($feed->last_modified);
 
-            /** @var \Zend\Feed\Reader\Reader $reader */
-            $reader = Yii::$app->feed->reader();
-            $res = $reader->import($feed->url, null, $last_modified);
+            try {
+                /** @var \Zend\Feed\Reader\Reader $reader */
+                $reader = Yii::$app->feed->reader();
+                $res = $reader->import($feed->url, null, $last_modified);
 
-            foreach ($res as $item) {
-                $diff = $item->getDateModified()->diff($last_modified);
-                if ($diff->format('%R') == '-') {
-                    $telegram->sendMessage(
-                        $item->getTitle() . PHP_EOL . $item->getLink()
-                    );
-
-                    $diff = $item->getDateModified()->diff($last_feed_date);
+                foreach ($res as $item) {
+                    $diff = $item->getDateModified()->diff($last_modified);
                     if ($diff->format('%R') == '-') {
-                        $last_feed_date = $item->getDateModified();
+                        $telegram->sendMessage(
+                            $item->getTitle() . PHP_EOL . $item->getLink()
+                        );
+
+                        $diff = $item->getDateModified()->diff($last_feed_date);
+                        if ($diff->format('%R') == '-') {
+                            $last_feed_date = $item->getDateModified();
+                        }
                     }
                 }
+            } catch (\Exception $e) {
+                echo date('Y-m-d H:i:s') . $e->getFile() . ":" . $e->getLine()
+                    . PHP_EOL . $e->getMessage() . PHP_EOL;
+            } finally {
+                $feed->last_modified = $last_feed_date->format('Y-m-d H:i:s');
+                $feed->save();
             }
-            $feed->last_modified = $last_feed_date->format('Y-m-d H:i:s');
-            $feed->save();
         }
     }
 }
